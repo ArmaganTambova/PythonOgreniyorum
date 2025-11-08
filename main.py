e7d415577bce85ea59a7f6519404b3e13e7a5e70
ArmaganTambova <armagantambova@gmail.com>
2025-11-08 11:41:22 +0300

    Add initial project files and main.py example
    Added PyCharm project configuration files and a main.py script with basic print statement and comments explaining Python's print function, type safety, and comment syntax.

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+# Python'da print() fonksiyonu konsola yazı yazdırmak için kullanılır.
+
+"""
+Python tür güvenliği bulunmayan bir dildir.
+Bu sebepten tür dönüşümlerinde ve gerekli veriler kullanılırken ekstradan dikkat edilerek,
+yazılımcı tarafından tür güvenliği sağlanmalıdır.
+"""
+
+# Python'da # işareti ile yorum satırı yapılır.
+
+"""
+Yan yana 3 adet çift tırnak ile yorum satırını açıp, tekrardan 3 adet çift tırnak ile kapatabilirsiniz.
+Bu şekilde açmış olduğunuz yorum satırını alt alta tekrardan başka bir şey eklemeden devam ettirebilirsiniz.
+"""
+
+'''
+Aynı işlemi tek tırnak ile de yapabilirsiniz.
+İllaki çift tırnak olmasına gerek yok.
+'''
